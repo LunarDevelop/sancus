abad790494583d72bcdce7a0eabe3ad3a9fd71fa
Benjamin Macaulay <bmacaulay4843@gmail.com>
2021-08-06 19:40:18 +0100

    Version 6 Interaction Update
    Help command assist to new cog object

--- lib/cogs/help/__init__.py
+++ lib/cogs/help/__init__.py
@@ -1,4 +1,5 @@
-from .help import Help
+from .help import Help, helpButtons
 
 def setup(client):
-    client.add_cog(Help(client))
+    #client.add_cog(Help(client))
+    client.add_cog(helpButtons(client))
