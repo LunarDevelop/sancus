5fec0538fca8ae56e4b6d020998590eab6f7f6d9
Benjamin Macaulay <bmacaulay4843@gmail.com>
2021-08-27 16:07:00 +0100

    Version 6 Interaction Update
    Comments added to the file

--- lib/cogs/misc/gifs.py
+++ lib/cogs/misc/gifs.py
@@ -1,4 +1,5 @@
 import discord
+from discord.embeds import Embed
 from discord.ext import commands
 import requests, json, random, io
 
@@ -10,26 +11,33 @@ class GIFS(commands.Cog):
     LMT = 8
 
     @commands.command()
-    async def gif(self, ctx, *, search_term):
-        if search_term is None:
-            await ctx.send("Please try again and enter word or phrase for the gif you want. So its something like this `s!gif good morning`")
+    async def gif(self, ctx, search_term):
+        f"""Search for a gif and {self.client.user.name} will return a random gif from the list
+        
+        Args:
+        
+        - Search term: what kind of gif do you want
+        """
 
+        #replace any spaces with nothing to remove them
         for char in search_term:
             if char in " ":
                 search_term = search_term.replace(char,'')
 
+        #set the url link with all of the required args
         url = self.RANDOMURL + "key=%s&q=%s&media_filter=%s&limit=%s&contentfilter=%s" % (self.APIKEY, search_term, "minimal", 10, "medium")
 
         r = requests.get(url)
 
-        if r.status_code == 200 or r.status_code == 202:
+        #check if requested info was code 200
+        if r.ok:
             try:
                 gif = json.loads(r.content)
                 giflist = gif['results']
-                ran = random.randint(0, len(giflist))
-                image = giflist[ran]['media']
-
-                await ctx.send(giflist[ran]['url'])
+                ran = random.randint(0, (len(giflist)-1))
+                image = giflist[ran]['url']
+                                
+                await ctx.send(image)
 
             except: await ctx.send("I can't find any gif matching that word")
 
@@ -37,4 +45,4 @@ class GIFS(commands.Cog):
             await ctx.send("Rate limit exceeded")
 
         else:
-            await ctx.send("GIF loading has failed contact Bot Owner")
+            await ctx.send("GIF loading has failed contact Bot Owner ( Solar#0404 )")
