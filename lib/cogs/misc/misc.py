3a252053f49f476585f7eaa2e4ed1ef61f9dc360
Benjamin Macaulay <bmacaulay4843@gmail.com>
2021-08-27 14:15:28 +0100

    Version 6 Interaction Update
    Updated github, invite and patreon
    All (apart from ping) are now embeds with the footer
    Info has been added
    botserver changed to support
    bot hugs stuff has been removed

--- lib/cogs/misc/misc.py
+++ lib/cogs/misc/misc.py
@@ -4,142 +4,183 @@ from discord.ext.commands import command, Cog
 from lib.bot import bot
 
 from datetime import datetime
-import json, requests
+import json
+import requests
 
-
-from .triva import Trivia
 from .voting import Polls
 from .gifs import GIFS
+Test_Servers = [780211278614364160]
 
 
-class Misc(Trivia,
-            Polls,
-            GIFS,
-            Cog):
+class Misc(
+        Polls,
+        GIFS,
+        Cog):
 
     def __init__(self, client):
         self.client = client
         self.config = bot.config
 
-    #Github command
-    #@commands.command()
-    async def gitlab(self, ctx):
-        await ctx.send("Check out my GitLab page, where you can find all of my code: https://tinyurl.com/sancus-gitlab")
+    @command()
+    async def github(self, ctx):
+        "GitHub repository link"
 
-    @command(name="invite",brief ="Invite Sancus to your server")
-    async def invite(self, ctx):
-        await ctx.send(f"{ctx.author.mention}, here is my invite link: https://tinylink.net/oJgq2 :space_invader:")
+        embed = Embed(
+            title="Bot Repository",
+            description=f"Want to see all the behind the scenes code for {self.client.user.mention}",
+            colour=0x0009900ff
+        )
 
-    @command(name="botserver", brief="Join the offical server for Sancus ")
-    async def botserver(self,ctx):
-        await ctx.send(f"{ctx.author.mention}, come and joining my official server: https://discord.gg/XZB8mnY6f8 :flag_white: ")
+        embed.set_thumbnail(
+            url="https://avatars.githubusercontent.com/u/18133?s=200&v=4")
+        embed.url = "https://github.com/Solar-Productions/sancus"
+
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
+
+        await ctx.send(embed=embed)
 
     @command()
-    async def hug(self, ctx, member : Member):
+    async def invite(self, ctx):
+        embed = Embed(
+            title=f"Invite {self.client.user.name} to your server",
+            description=f"Get access to the bot on your own server and use all the features",
+            url="https://bit.ly/sancus404",
+            colour=0x0009900ff
+        )
+
+        embed.set_thumbnail(url=self.client.user.display_avatar.url)
+
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
+
+        await ctx.send(embed=embed)
+
+    @command()
+    async def support(self, ctx):
+        embed = Embed(
+            title="Support??",
+            description="Do you want to check out the offical support server or get some new software for yourself?",
+            url="https://discord.gg/XZB8mnY6f8",
+            colour=0x0009900ff
+        )
+
+        embed.set_thumbnail(url=self.client.user.display_avatar.url)
+
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
+
+        await ctx.send(embed=embed)
+
+###
+
+    @command()
+    async def hug(self, ctx, member: Member):
+        """Hug a user by simply tagging them in the command
+
+        **Args:**
+
+        Member: Tag the user you want to hug"""
+
         url = "https://nekos.life/api/v2/img/hug"
 
         r = requests.get(url=url).json()
 
         embed = Embed(
             description=f"{member.mention} you have received a hug by {ctx.author.mention}",
-            colour = bot.oldConfig.embed(ctx.guild.id, "anime_image_nekopara")
+            colour=bot.oldConfig.embed(ctx.guild.id, "anime_image_nekopara")
         )
 
         embed.set_image(url=r["url"])
 
-        embed.set_footer(text = f"{datetime.utcnow().strftime('%B %d %Y - %H:%M UTC')}")
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
 
         await ctx.send(embed=embed)
 
     @command(name="pat")
-    async def _pat(self,ctx, member : Member):
+    async def _pat(self, ctx, member: Member):
+        """Pat some on the back or head
+
+        **Args:**
+
+        Member: Tag the user you want to pat"""
+
         url = "https://nekos.life/api/v2/img/pat"
 
         r = requests.get(url=url)
 
         embed = Embed(
             description=f"{member.mention} you have received a pat by {ctx.author.mention}",
-            colour = bot.oldConfig.embed(ctx.guild.id, "anime_image_nekopara")
+            colour=bot.oldConfig.embed(ctx.guild.id, "anime_image_nekopara")
         )
 
         embed.set_image(url=json.loads(r.content.decode("UTF-8"))['url'])
 
-        embed.set_footer(text = f"{datetime.utcnow().strftime('%B %d %Y - %H:%M UTC')}")
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
 
         await ctx.send(embed=embed)
 
     @command()
-    async def bothug(self, ctx, *, member : Member):
-        await ctx.send(f'{member.mention}, you have been sent a hug from <@341999214043332619>')
-    
-    @command()
-    async def bothugself(self, ctx):
-        await ctx.send(f'<@341999214043332619>, has received a hug from him self.')
+    async def slap(self, ctx, *, member: Member):
+        """Slap someone's face
 
-    @command()
-    async def hugself(self, ctx):
-        await ctx.send(f'{ctx.author.mention}, has hugged themself.')
+        **Args:**
 
-    @command()
-    async def slap(self, ctx, *, member : Member):
+        Member: Tag the user you want to slap"""
+
         url = "https://nekos.life/api/v2/img/slap"
 
         r = requests.get(url=url)
 
         embed = Embed(
             description=f"{member.mention} you have received a slap by {ctx.author.mention}",
-            colour = bot.oldConfig.embed(ctx.guild.id, "anime_image_nekopara")
+            colour=bot.oldConfig.embed(ctx.guild.id, "anime_image_nekopara")
         )
 
         embed.set_image(url=json.loads(r.content.decode("UTF-8"))['url'])
 
-        embed.set_footer(text = f"{datetime.utcnow().strftime('%B %d %Y - %H:%M UTC')}")
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
 
         await ctx.send(embed=embed)
 
-    #@command()
-    async def botlisting(self, ctx):
-        embed = Embed(
-            title = f"Sancus is avaliable on the following listing sites",
-            colour = 0x000000
-        )
-
-    #Ping command
+    # Ping command
     @command()
     async def ping(self, ctx):
-        """
-            Gets the ping of the bot
-        """
+        "Gets the ping of the bot"
 
         await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
         print(f'Pong! {round(self.client.latency * 1000)}ms')
 
     @command()
     async def patreon(self, ctx):
+        embed = Embed(
+            title="Patreon Perks",
+            description=f"Help support Sancus and the others in their continued adventure to climb the ladder.",
+            url=" https://www.patreon.com/solarbam",
+            colour=0x0009900ff
+        )
 
-        await ctx.send("Here is my patreon page: https://www.patreon.com/solarbam")
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/attachments/789247201678327838/880746027999690772/Digital-Patreon-Logo_FieryCoral.png")
+
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
+
+        await ctx.send(embed=embed)
 
     @command()
-    async def privicy(self, ctx):
+    async def info(self, ctx):
         embed = Embed(
-            title = f"Privicy Policy",
-            #description = "Sancus only stores the user ID of each user who uses any of the economy commands. By using the commands you agree to have sancus store the information. Sancus also stores guild id numbers in order to provide customise features for each server. By using Sancus on your discord server you agree to have the guild id number stored.\n To remove any information stored by the bot please message SolarBAM404#1179 and he will instantly remove the information.",
-            colour = 0x000000
+            title="About me | The creator, Solar",
+            description="""I started programming stuff in Secondary School here in the UK. Sancus was my first big project and the first libray that I understood. He is also made the ground work for me to go out and learn serval other things. For example learn HTML, CSS and JS to make the website (still working on it :P ) and C# to make the API link to my database. Along side many other things. 
+            \n I inspire to continue coding and make some sort of living of it. Current trying to find a starting place for all of that but I love coding and love being a part of the amazing community
+            \n\n The idea behind Sancus was orginally just to have a bit of fun and make a cool bot for my mate. I then decided to expand upon that and make a full functional bot with tons of features and make it public, which I believe I am getting very close to fully functional personally. There are still sections which need to fined tuned but so far it is all okay."""
         )
 
-        embed.add_field(
-            name="Types of data",
-            value="Information that is stores are stored for the use within features that are provided by the bot. \nThis includes: \n1) User ID numbers, \n2) Guild IDs",
-            inline= False
-        )
-
-        embed.add_field(
-            name= "Processing the data",
-            description = "The data is kept and stored within a secure area that can only be accessed through the bot it self. The data is automatically taken once the bot has joined a guild (for Guild ID) and once a user has used a command which requires storing the user ID. Types of commands which need user ID are stated below. No data is used by third-party applications",
-            inline = False
-        )
-
-
-        embed.set_thumbnail(url=self.client.user.avatar_url)
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
 
         await ctx.send(embed=embed)
