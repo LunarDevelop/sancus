d0c852179536da617f729e9a790d27adff72468a
Benjamin Macaulay <bmacaulay4843@gmail.com>
2021-08-27 14:22:22 +0100

    Version 6 Interaction Update
    Comments added for commands
    Footer added to embeds

--- lib/cogs/logging/members.py
+++ lib/cogs/logging/members.py
@@ -1,5 +1,7 @@
 from configparser import ConfigParser
-import discord, io, requests
+import discord
+import io
+import requests
 from discord.ext.commands import Cog
 from discord import Embed, File
 from datetime import datetime
@@ -7,12 +9,13 @@ from datetime import datetime
 from lib.bot import bot
 from functions.exceptions import NoLogChannel
 
+
 class Members(Cog):
 
     def __init__(self, client):
         self.client = client
 
-    #When Player joins message
+    # When Player joins message
     @Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
@@ -22,14 +25,17 @@ class Members(Cog):
         userurl = member.display_avatar.url
 
         embed = Embed(
-            title = f'{member.name}#{member.discriminator} ({member.id}) has joined the guild, {guild.name}',
-            description = member.mention,
+            title=f'{member.name}#{member.discriminator} ({member.id}) has joined the guild, {guild.name}',
+            description=member.mention,
             colour=0x0000203F9
         )
 
         embed.set_thumbnail(url=userurl)
         embed.timestamp = datetime.utcnow()
-        embed.add_field(name="Total Users:", value=total_users,inline=True)
+        embed.add_field(name="Total Users:", value=total_users, inline=True)
+
+        embed.set_footer(text=self.client.embedAuthorName,
+                         icon_url=self.client.embedAuthorUrl)
 
         try:
             await (await self.client.getLogChannel(guild.id)).send(embed=embed)
@@ -37,26 +43,30 @@ class Members(Cog):
         except:
             pass
 
-    #When Player leaves message
+    # When Player leaves message
     @Cog.listener()
     async def on_member_remove(self, member):
-        
+
         guild = member.guild
 
-        channel =  await self.client.getLogChannel(guild.id)
-        
+        channel = await self.client.getLogChannel(guild.id)
+
         userurl = member.display_avatar.url
 
         userleft = Embed(
-            title = f'{member.name}#{member.discriminator} ({member.id}) has left the guild, {guild.name}',
-            description = f'{member.mention}',
-            colour = 0x000e00101
+            title=f'{member.name}#{member.discriminator} ({member.id}) has left the guild, {guild.name}',
+            description=f'{member.mention}',
+            colour=0x000e00101
         )
 
         userleft.set_thumbnail(url=userurl)
 
+        userleft.set_footer(text=self.client.embedAuthorName,
+                            icon_url=self.client.embedAuthorUrl)
+
         await channel.send(embed=userleft)
 
+    #If nickname changes
     @Cog.listener()
     async def on_member_update(self, before, after):
         channel = await self.client.getLogChannel(before.guild.id)
@@ -64,11 +74,14 @@ class Members(Cog):
         if before.nick != after.nick:
 
             embed = Embed(
-                title = f'{before.name} changed their nickname',
-                description = f"\n**BEFORE:** {before.nick}\n\n**AFTER:** {after.nick}",
-                colour = 0x00002ee00
+                title=f'{before.name} changed their nickname',
+                description=f"\n**BEFORE:** {before.nick}\n\n**AFTER:** {after.nick}",
+                colour=0x00002ee00
             )
 
             embed.set_thumbnail(url=before.display_avatar.url)
+
+            embed.set_footer(text=self.client.embedAuthorName,
+                             icon_url=self.client.embedAuthorUrl)
 
             await channel.send(embed=embed)
