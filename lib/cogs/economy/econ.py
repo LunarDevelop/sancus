46c98ce8065aa509442ad6c49f9ca4fe0d94600a
Benjamin Macaulay <bmacaulay4843@gmail.com>
2021-08-30 02:08:15 +0100

    Readable update

--- lib/cogs/economy/econ.py
+++ lib/cogs/economy/econ.py
@@ -2,37 +2,42 @@ import json
 
 import discord
 
-from lib.bot import bot 
+from lib.bot import bot
+
 
 def open_econ():
     with open("./data/econ.json") as f:
         return json.load(f)
 
+
 def save_econ(data):
     with open("./data/econ.json", 'w') as f:
-        json.dump(data, f, indent=4 )
+        json.dump(data, f, indent=4)
+
 
 def bank_add_user(userID):
     data = open_econ()
 
     if str(userID) not in data['bank']:
         data['bank'][userID] = {
-            'bank' : 0,
-            'wallet' : 0
-        } 
-    
+            'bank': 0,
+            'wallet': 0
+        }
+
     save_econ(data)
+
 
 def shop_add_user(userID):
     data = open_econ()
 
     if str(userID) not in data['shop']:
         data['shop'][userID] = {
-            'inventory' : {},
-            'amount' : 0
-        } 
-    
+            'inventory': {},
+            'amount': 0
+        }
+
     save_econ(data)
+
 
 class bank():
 
@@ -56,5 +61,5 @@ class bank():
         bot.oldConfig.set_user(self.member, 'wallet', total)
 
     def remove_bank_money(self, amount):
-        total = int(self.user['bank']) +- amount
+        total = int(self.user['bank']) + - amount
         bot.oldConfig.set_user(self.member, 'bank', total)
