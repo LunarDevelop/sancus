583c47c15b8021c47dddb035870f3326046a9f60
Benjamin Macaulay <bmacaulay4843@gmail.com>
2021-08-30 19:19:11 +0100

    Moderation added with anti mention spam detection

---
+++ lib/cogs/mod/moderation.py
@@ -0,0 +1,18 @@
+import discord
+from discord.abc import Message
+from discord.ext.commands import Cog
+
+class antispam(Cog):
+    
+    def __init__(self, client) -> None:
+        super().__init__()
+        
+        self.client = client
+        
+        
+    @Cog.listener(name="Anti Mention Spam")
+    async def on_message(self, msg:Message):
+        mentionList = msg.mentions
+        
+        if len(mentionList) > 5:
+            await msg.delete()
