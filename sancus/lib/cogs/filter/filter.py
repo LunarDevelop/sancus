1518f8669ff92312226e1156b2a7907bddff4544
Solar <bmacaulay4843@gmail.com>
2021-10-03 11:57:05 +0100

    Fixed a small bug with setting up a custome filter
    When setting the custom filter it would try to delete the message from the filter
    So I have fixed that by adding a try/excpet statement and if NotFound return nothing

--- sancus/lib/cogs/filter/filter.py
+++ sancus/lib/cogs/filter/filter.py
@@ -1,5 +1,6 @@
 import discord
 from discord import Embed
+from discord.errors import NotFound
 from discord.ext import commands
 import requests
 import asyncio
@@ -9,7 +10,7 @@ from lib.bot import bot
 
 class Filter(commands.Cog):
 
-    def __init__(self, client : bot):
+    def __init__(self, client: bot):
         self.client = client
 
     @commands.Cog.listener()
@@ -18,21 +19,24 @@ class Filter(commands.Cog):
             check = requests.get(
                 f"https://www.purgomalum.com/service/containsprofanity?text=${message.content}")
             check = check.text
-                    
+
             if str(message.guild.id) in self.client.guilds_:
                 guild = self.client.guilds_[str(message.guild.id)]
                 filter = guild["filter"]
                 words = guild["filterWords"]
                 delete = guild["filterDelete"]
-            
+
             else:
                 return
 
             if filter:
                 if check == 'true':
                     if delete == True:
-                        await message.delete()
-                        
+                        try:
+                            await message.delete()
+                        except NotFound:
+                            return
+
                     elif delete == False:
                         response = requests.get(
                             "https://insult.mattbas.org/api/insult")
@@ -42,10 +46,13 @@ class Filter(commands.Cog):
                             description=response.text
                         )
                         await message.channel.send(embed=embed)
-                        
+
                 elif message.content in words:
                     if delete == True:
-                        await message.delete()
+                        try:
+                            await message.delete()
+                        except NotFound:
+                            return
 
                     elif delete == False:
                         response = requests.get(
