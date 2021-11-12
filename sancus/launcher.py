a94f2f168236aee0d41ff183162cae7f963800da
Solar <solar@lunar-dev.com>
2021-11-12 00:18:36 +0000

    Removal of question and updating.
    This was done to allow for using Docker containers to manage bots on VM

--- sancus/launcher.py
+++ sancus/launcher.py
@@ -3,43 +3,48 @@ from time import sleep
 import json
 import configparser
 
+import sys, os
+
+try:
+    sys.path.insert(0, os.path.join(os.getcwd(),"sancus"))
+except:pass
+
 from functions.consoleColours import *
 
-###Getting Version
-config = configparser.ConfigParser()
-with open("sancus/data/config.ini", 'r') as configFile:
-    config.read_file(configFile)
-Version = config.get("DEFAULT", "version")
+class main():
+    ###Getting Version
+    config = configparser.ConfigParser()
+    with open("sancus/data/config.ini", 'r') as configFile:
+        config.read_file(configFile)
+    Version = config.get("DEFAULT", "version")
 
-
-updateAsk = input("Do you want to update your packages?\n ")
-if updateAsk.lower() in ["yes", "y"]:
-    os.system("py update.script.py")
-
-###Launching bot
-try:
-    print(f"{colours.PURPLE}Starting bot.......{colours.ENDC}")
-    from lib.bot import bot
-    bot.run(Version)
-
-except ModuleNotFoundError:
-    print(f"{colours.WARNING}Module not detected, \n{colours.OKCYAN} Installing modlues now {colours.ENDC}")
-    os.system("py install-script.py")
-    print(f"{colours.PURPLE}Starting bot.......{colours.ENDC}")
-    from lib.bot import bot
-    bot.run(Version)
-
-except TypeError as error:
-    print(colours.FAIL, error, colours.ENDC)
-
-except:
+    ###Launching bot
     try:
-        print(f"{colours.WARNING}Trying to start bot, attepmt #2.......{colours.ENDC}")
+        print(f"{colours.PURPLE}Starting bot.......{colours.ENDC}")
         from lib.bot import bot
         bot.run(Version)
 
-    except:
-        print(colours.FAIL, "Failed to start", colours.ENDC)
+    except ModuleNotFoundError:
+        print(f"{colours.WARNING}Module not detected, \n{colours.OKCYAN} Installing modlues now {colours.ENDC}")
+        os.system("py install-script.py")
+        print(f"{colours.PURPLE}Starting bot.......{colours.ENDC}")
+        from lib.bot import bot
+        bot.run(Version)
 
-from lib.bot import bot
-bot.run(Version)
+    except TypeError as error:
+        print(colours.FAIL, error, colours.ENDC)
+
+    except:
+        try:
+            print(f"{colours.WARNING}Trying to start bot, attepmt #2.......{colours.ENDC}")
+            from lib.bot import bot
+            bot.run(Version)
+
+        except:
+            print(colours.FAIL, "Failed to start", colours.ENDC)
+
+    from lib.bot import bot
+    bot.run(Version)
+
+if __name__ == "__main__":
+    main()
