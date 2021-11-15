16dff3548dadef59d0f3afc9b846b31d87448a5c
Solar <solar@lunar-dev.com>
2021-11-15 20:29:08 +0000

    Allowed for multiple roles to be added at once, manage_roles permission is need to use command Updated docstring a bit

--- sancus/lib/cogs/mod/usercmds.py
+++ sancus/lib/cogs/mod/usercmds.py
@@ -1,3 +1,4 @@
+from discord.ext.commands.core import has_permissions
 from discord.member import Member
 from discord.ext.commands import Cog, command
 from discord.role import Role
@@ -12,18 +13,24 @@ class userCmds(Cog):
         self.client : Bot = client
 
     @command()
-    async def give(self, ctx : Context, user : Member, *, role : Role=""):
-        """This command will give server admins access to give users roles
-        by simply tagging the user and then the role.
+    @has_permissions(manage_roles=True)
+    async def give(self, ctx : Context, user : Member, *, roles : str):
+        """This command will give server admins access to give roles to users
+        by simply tagging the user and then the roles that you want them to have.
 
         You can tag a user who's not in the channel by doing the follow:
         `<@userID>` and replacing userID with their id number
 
         And you can tag a role using `<@&roleID>` if you are unable to tag the role normally
+        Don't worry if a user already has one of the roles they will be skipped by the system
         """
+        roles = roles.split(" ")
+        roleList = ""
 
-        if role in user.roles:
-            return await ctx.send("User already has that role.")
+        for role in roles:
+            role = role.replace("<@&", " ").replace(">", " ")
 
-        await user.add_roles(role)
-        return await ctx.send(f"{user.mention} has been given the {role.mention} role")
+            role = ctx.guild.get_role(int(role))
+            await user.add_roles(role)
+            roleList += role.name +", "
+        return await ctx.send(f"{user.mention} has been given the following roles: {roleList}")
