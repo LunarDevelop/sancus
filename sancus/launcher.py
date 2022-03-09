f2c0c2772e589953b923375e4a7ce8187f8f632c
Solar <solar@lunar-dev.com>
2022-03-09 00:44:35 +0000

    New launcher file
    This is to run the new version 7 of Sancus
    Only contains the run line

--- sancus/launcher.py
+++ sancus/launcher.py
@@ -1,24 +1,9 @@
-import os
 from time import sleep
-import json
-import configparser
 
-import sys, os
-
-try:
-    sys.path.insert(0, os.path.join(os.getcwd(),"sancus"))
-except:pass
-
-from functions.consoleColours import *
+from lib import bot
 
 class main():
+    bot.run("7.0.0")   
     
-    from functions.mongoDbAPI import connectionDb
-    Version = connectionDb().get_config_config()["version"]
-
-
-    from lib.bot import bot
-    bot.run(Version)
-
 if __name__ == "__main__":
     main()
