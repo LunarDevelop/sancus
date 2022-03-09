e157484f624726bfd2dc85f41f042c25846ec5bc
Solar <solar@lunar-dev.com>
2022-03-09 13:57:33 +0000

    Updated with new colours and fileMode can be changed

--- sancus/misc/log.py
+++ sancus/misc/log.py
@@ -9,14 +9,32 @@ pyLogging.addLevelName(pyLogging.DEBUG+5, "GuildLogging")
 
 GUILDLOGGING = pyLogging.DEBUG+5
 
+
 class CustomFormatter(pyLogging.Formatter):
 
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
     green = "\033[92m"
+    black = "\u001b[30m"
+    red = "\u001b[31m"
+    bold_red = "\x1b[31;1m"
+    green = "\u001b[32m"
+    yellow = "\u001b[33m"
+    blue = "\u001b[34m"
+    magenta = "\u001b[35m"
+    cyan = "\u001b[36m"
+    white = "\u001b[37m"
+    grey = "\x1b[38;20m"
+    
+    brightBlack = "\u001b[30;1m"
+    brightRed = "\u001b[31;1m"
+    brightGreen = "\u001b[32;1m"
+    brightYellow = "\u001b[33;1m"
+    brightBlue = "\u001b[34;1m"
+    brightMagenta = "\u001b[35;1m"
+    brightCyan = "\u001b[36;1m"
+    brightWhite = "\u001b[37;1m"
+    
+    reset = "\u001b[0m"
+    
     format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
@@ -24,7 +42,8 @@ class CustomFormatter(pyLogging.Formatter):
         pyLogging.INFO: green + format + reset,
         pyLogging.WARNING: yellow + format + reset,
         pyLogging.ERROR: red + format + reset,
-        pyLogging.CRITICAL: bold_red + format + reset
+        pyLogging.CRITICAL: bold_red + format + reset,
+        GUILDLOGGING: cyan + format + reset,
     }
 
     def format(self, record):
@@ -34,7 +53,13 @@ class CustomFormatter(pyLogging.Formatter):
         return formatter.format(record)
 
 
-def logging(name: str, level=pyLogging.WARNING, file: bool = False, fileName: str = "latest") -> pyLogging:
+def logging(
+    name: str, 
+    level=pyLogging.WARNING, 
+    file: bool = False, 
+    fileName: str = "latest", 
+    fileMode: str = 'w'
+    ) -> pyLogging:
 
     logger = pyLogging.getLogger(name)
     logger.setLevel(level)
@@ -45,7 +70,7 @@ def logging(name: str, level=pyLogging.WARNING, file: bool = False, fileName: st
 
     # Create formatters and add it to handlers
     if file:
-        f_handler = pyLogging.FileHandler(f'logs/{fileName}.log', 'w')
+        f_handler = pyLogging.FileHandler(f'logs/{fileName}.log', fileMode)
         f_format = pyLogging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s',  datefmt='%d-%b-%y %H:%M:%S')
         f_handler.setFormatter(f_format)
